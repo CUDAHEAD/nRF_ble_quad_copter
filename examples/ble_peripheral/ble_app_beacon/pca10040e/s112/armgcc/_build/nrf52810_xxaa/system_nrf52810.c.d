c68120890ab5083677394c3ec4f764e1bbcdabb4
efraimreshef@gmail.com <efraimreshef@gmail.com>
2020-11-02 21:11:56 +0200

    blinky_working

---
+++ examples/ble_peripheral/ble_app_beacon/pca10040e/s112/armgcc/_build/nrf52810_xxaa/system_nrf52810.c.d
@@ -0,0 +1,62 @@
+_build/nrf52810_xxaa/system_nrf52810.c.o: \
+ ../../../../../../modules/nrfx/mdk/system_nrf52810.c \
+ /home/eduards/gnuarmemb/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h \
+ /home/eduards/gnuarmemb/arm-none-eabi/include/stdint.h \
+ /home/eduards/gnuarmemb/arm-none-eabi/include/machine/_default_types.h \
+ /home/eduards/gnuarmemb/arm-none-eabi/include/sys/features.h \
+ /home/eduards/gnuarmemb/arm-none-eabi/include/_newlib_version.h \
+ /home/eduards/gnuarmemb/arm-none-eabi/include/sys/_intsup.h \
+ /home/eduards/gnuarmemb/arm-none-eabi/include/sys/_stdint.h \
+ /home/eduards/gnuarmemb/lib/gcc/arm-none-eabi/9.2.1/include/stdbool.h \
+ ../../../../../../modules/nrfx/mdk/nrf.h \
+ ../../../../../../modules/nrfx/mdk/nrf52810.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmInstr.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmFunc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmSimd.h \
+ ../../../../../../modules/nrfx/mdk/system_nrf52810.h \
+ ../../../../../../modules/nrfx/mdk/nrf52810_bitfields.h \
+ ../../../../../../modules/nrfx/mdk/nrf51_to_nrf52810.h \
+ ../../../../../../modules/nrfx/mdk/nrf52_to_nrf52810.h \
+ ../../../../../../modules/nrfx/mdk/compiler_abstraction.h
+
+/home/eduards/gnuarmemb/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h:
+
+/home/eduards/gnuarmemb/arm-none-eabi/include/stdint.h:
+
+/home/eduards/gnuarmemb/arm-none-eabi/include/machine/_default_types.h:
+
+/home/eduards/gnuarmemb/arm-none-eabi/include/sys/features.h:
+
+/home/eduards/gnuarmemb/arm-none-eabi/include/_newlib_version.h:
+
+/home/eduards/gnuarmemb/arm-none-eabi/include/sys/_intsup.h:
+
+/home/eduards/gnuarmemb/arm-none-eabi/include/sys/_stdint.h:
+
+/home/eduards/gnuarmemb/lib/gcc/arm-none-eabi/9.2.1/include/stdbool.h:
+
+../../../../../../modules/nrfx/mdk/nrf.h:
+
+../../../../../../modules/nrfx/mdk/nrf52810.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cm4.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cmInstr.h:
+
+../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cmFunc.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cmSimd.h:
+
+../../../../../../modules/nrfx/mdk/system_nrf52810.h:
+
+../../../../../../modules/nrfx/mdk/nrf52810_bitfields.h:
+
+../../../../../../modules/nrfx/mdk/nrf51_to_nrf52810.h:
+
+../../../../../../modules/nrfx/mdk/nrf52_to_nrf52810.h:
+
+../../../../../../modules/nrfx/mdk/compiler_abstraction.h:
